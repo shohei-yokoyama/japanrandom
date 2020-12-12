4e62fe9b365722043283fd0b2facb1ca0fe83237
YKS <sp658qy9@gmail.com>
2020-12-13 00:09:36 +0900

    google

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="google-site-verification" content="JQfrPqskpklrH8bMtAWftQgnQWFk3HSmeQ9kbJg9UCc" />
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
   <link href="{{ mix('css/style.css') }}" rel="stylesheet">
   <link rel="preconnect" href="https://fonts.gstatic.com">
